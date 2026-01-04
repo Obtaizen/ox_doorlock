6fc733e00848c26a605261076af9229499e48dae
Josh <57309415+Obtaizen@users.noreply.github.com>
2026-01-04 13:35:08 +0000

    ox_doorlock - Updated stream

--- config.lua
+++ config.lua
@@ -11,10 +11,10 @@ Config.DrawTextUI = false
 ---Set the properties used by [DrawSprite](https://docs.fivem.net/natives/?_0xE7FFAE5EBF23D890).
 Config.DrawSprite = {
     -- Unlocked
-    [0] = { 'mpsafecracking', 'lock_open', 0, 0, 0.018, 0.018, 0, 255, 255, 255, 100 },
+    [0] = { 'ob-textures', 'Key-E-Green',  0, 0, 0.018, 0.018, 0, 110, 210, 120, 190 },
 
     -- Locked
-    [1] = { 'mpsafecracking', 'lock_closed', 0, 0, 0.018, 0.018, 0, 255, 255, 255, 100 },
+    [1] = { 'ob-textures', 'Key-E-Red', 0, 0, 0.018, 0.018, 0, 255, 90, 120, 190 },
 }
 
 ---Allow the specified ace principal to use 'command.doorlock'.
